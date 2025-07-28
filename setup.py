326bd3be950d580f670dd3b2a180643f9a09d4af
emaoumaima <sellouane.oumaima@gmail.com>
2025-07-28 23:23:13 +0100

    Add packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="poke_pipeline",
+    version="0.1",
+    packages=find_packages(),
+        install_requires=[
+        "pandas>=1.5"
+    ]
+)
